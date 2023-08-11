3f7961b6e5201c5511c1707475ee540f208c4e6f
Janice Pickron <janpickron@yahoo.com>
2023-08-11 16:24:26 -0400

    add new files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [menuItems, setMenuItems] = useState([]);
+  const [form, setForm] = useState({});
+  const [itemToUpdate, setItemToUpdate] = useState('')
+
+  useEffect(() => {
+    // fetch in local API
+    fetch("http://localhost:5500/menu")
+      // get response and convert to JSON
+      .then((apiResponse) => apiResponse.json())
+      .then((cleanJson) => setMenuItems(cleanJson))
+      .catch((myError) => console.log(myError));
+  }, [menuItems]);
+
+  const handleAddMenuItem = (e) => {
+    // stop from refreshing browser
+    e.preventDefault();
+
+    //const form = {title: title, description: description}
+
+    fetch("http://localhost:5500/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(form),
+    })
+      .then((res) => res.json())
+      .then((data) => setMenuItems(data))
+      .catch((myError) => console.log(myError));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Menu items</h1>
+      <input type="text" name="title" placeholder="Title" />
+      <input type="text" name="description" placeholder="Description" />
+      <button onClick={handleAddMenuItem}>Add</button>
+      <p>Item clicked with</p>
+      <ul>
+        {menuItems.map((item) => {
+          return (
+            <li onClick={() => setItemToUpdate(item.title)}> {item.title}</li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
-
-export default App;
