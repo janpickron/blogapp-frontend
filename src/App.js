f7091677b3fd48308586b7448aa3d8cd3f1c0eea
Janice Pickron <janpickron@yahoo.com>
2023-08-14 19:25:17 -0400

    update

--- src/App.js
+++ src/App.js
@@ -69,7 +69,7 @@ export default function App() {
       description: ""
     });
   }
-
+// Delete function 
   const handleDeleteMenuItem = (e) => {
     // stop from refreshing browser
     e.preventDefault()
